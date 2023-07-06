af0621178c8a68820c11c3a0990b6d3f058437d9
nora169 <nouraibrahem395@gmail.com>
2023-07-06 22:47:02 +0300

    creating project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The Hangman program randomly selects a secret word from a list of secret words.',
+    author='Me',
+    license='',
+)
